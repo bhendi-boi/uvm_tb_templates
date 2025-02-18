85b8e84120e0bc55de21245e184132bbbea41934
bhendi-boi <bhendiisgreen@gmail.com>
2025-02-18 10:49:54 +0530

    Added the ability to make an agent active or passive - use the set_active_or_passive method on agent class to control the behaviour

--- agent.sv
+++ agent.sv
@@ -1,8 +1,11 @@
 class agnt extends uvm_agent;
     `uvm_component_utils(agnt)
 
-    drv  driver;
-    mon  monitor;
+    // agent defaults to active
+    uvm_active_passive_enum is_active = UVM_ACTIVE;
+
+    drv driver;
+    mon monitor;
     seqr sequencer;
 
     function new(string name = "agnt", uvm_component parent);
@@ -13,16 +16,27 @@ class agnt extends uvm_agent;
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
         `uvm_info("Agent", "Build phase agent", UVM_HIGH)
-        driver = drv::type_id::create("driver", this);
-        // it is very unlikely that you'll extend a sequencer
-        sequencer = new("sequencer", this);
+
+        if (this.is_active == UVM_ACTIVE) begin
+            driver = drv::type_id::create("driver", this);
+            // it is very unlikely that you'll extend a sequencer
+            sequencer = new("sequencer", this);
+        end
+
         monitor = mon::type_id::create("monitor", this);
     endfunction
 
     function void connect_phase(uvm_phase phase);
         super.connect_phase(phase);
         `uvm_info("Agent", "Connect phase agent", UVM_HIGH)
-        driver.seq_item_port.connect(sequencer.seq_item_export);
+
+        if (this.is_active == UVM_ACTIVE) begin
+            driver.seq_item_port.connect(sequencer.seq_item_export);
+        end
+    endfunction
+
+    function void set_active_or_passive(int active_or_passive);
+        this.is_active = active_or_passive ? UVM_ACTIVE : UVM_PASSIVE;
     endfunction
 
 endclass
