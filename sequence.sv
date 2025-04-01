0a1902b2719094e98fe74445be3e70c628fc05ae
bhendi-boi <bhendiisgreen@gmail.com>
2025-04-01 09:45:39 +0530

    updated sequence classes

--- sequence.sv
+++ sequence.sv
@@ -1,5 +1,5 @@
-class div_by_mul_2_seq extends uvm_sequence;
-    `uvm_object_utils(div_by_mul_2_seq)
+class base_seq extends uvm_sequence;
+    `uvm_object_utils(base_seq)
 
     transaction tr;
     int no_of_tr;
@@ -8,13 +8,10 @@ class div_by_mul_2_seq extends uvm_sequence;
         this.tr.divisor < this.tr.dividend;
     }
 
-    constraint divisor_is_multiple_of_2 {this.tr.divisor % 2 == 0;}
-
-    function new(string name = "div_by_mul_2_seq");
+    function new(string name = "base_seq");
         super.new(name);
         this.set_no_of_tr(1);
-        `uvm_info("Div By Multiple of 2", "Constructed div_by_mul_2_seq",
-                  UVM_HIGH)
+        `uvm_info("Base Seq", "Constructed base_seq", UVM_HIGH)
     endfunction
 
     function void set_no_of_tr(int no_of_tr);
@@ -23,15 +20,20 @@ class div_by_mul_2_seq extends uvm_sequence;
 
     task body();
         tr = transaction::type_id::create("tr");
-
-
         repeat (no_of_tr) begin
             start_item(tr);
-            if (!tr.randomize())
-                `uvm_fatal("Dummy Sequence", "Randomisation failed")
+            if (!tr.randomize()) `uvm_fatal("Sequence", "Randomisation failed")
             finish_item(tr);
         end
     endtask
+
+endclass
+
+
+class div_by_mul_2_seq extends base_seq;
+    `uvm_object_utils(div_by_mul_2_seq)
+
+    constraint divisor_is_multiple_of_2 {this.tr.divisor % 2 == 0;}
 
 endclass
 
