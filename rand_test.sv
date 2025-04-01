9bf2f0aeb8f95aea9ab838c73e32953bf44cef0a
bhendi-boi <bhendiisgreen@gmail.com>
2025-04-01 09:30:53 +0530

    updated `rand_test`

--- rand_test.sv
+++ rand_test.sv
@@ -2,7 +2,7 @@ class rand_test extends uvm_test;
     `uvm_component_utils(rand_test)
 
     env environment;
-    // ? STEP 9: Declare sequences
+    div_by_mul_2_seq s0;
 
     function new(string name = "rand_test", uvm_component parent);
         super.new(name, parent);
@@ -11,7 +11,7 @@ class rand_test extends uvm_test;
 
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        `uvm_info("Rand Test", "Build phase environment", UVM_HIGH)
+        `uvm_info("Rand Test", "Build phase Rand Test", UVM_HIGH)
         environment = env::type_id::create("env", this);
     endfunction
 
@@ -20,15 +20,10 @@ class rand_test extends uvm_test;
         phase.raise_objection(this);
 
         // ? instantiate sequences
+        s0 = div_by_mul_2_seq::type_id::create("div_by_mul_2_seq");
+        s0.set_no_of_tr(2);
 
-        // Set no of transaction a sequence should generate
-        //  example syntax
-        // dummy_sequence.set_no_of_tr(1024);
-
-        // ? start them on sequencer
-        // dummy_seqence.start(environment.agent.sequencer);
-
-        // ? add any extra simulation delay
+        s0.start(environment.agent.sequencer);
 
         phase.drop_objection(this);
     endtask
