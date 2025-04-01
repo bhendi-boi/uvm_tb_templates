0cd97bb61c7405df98445f8fc446b48f43e3f0a9
bhendi-boi <bhendiisgreen@gmail.com>
2025-04-01 11:04:21 +0530

    using different functions for `quotient` and `remainder`

--- scoreboard.sv
+++ scoreboard.sv
@@ -1,15 +1,21 @@
-// ? Step 12: import the model function with appropriate args and return type
-// import "DPI-C" function int model(//args);
+
+import "DPI-C" function int find_quotient(
+    input int divisor,
+    input int dividend
+);
+import "DPI-C" function int find_remainder(
+    input int divisor,
+    input int dividend
+);
 class scb extends uvm_scoreboard;
     `uvm_component_utils(scb)
 
-    // ? STEP 7: Change the analysis imp port if you want
     uvm_analysis_imp #(transaction, scb) scoreboard_port;
     transaction trs[$];
     transaction tr;
     int i = 0;
 
-    // Declare variables which may be needed to make comparison
+
 
     function new(string name = "scb", uvm_component parent);
         super.new(name, parent);
@@ -24,8 +30,12 @@ class scb extends uvm_scoreboard;
     endfunction
 
     function void compare(transaction tr);
-        // Make a call to the model function written in step 11
-        // ? Fill this method with your comparison logic
+        int quotient, remainder;
+        quotient = find_quotient(tr.divisor, tr.dividend);
+        `uvm_info("Scoreboard", $sformatf("Quotient = %d", quotient), UVM_NONE)
+        remainder = find_remainder(tr.divisor, tr.dividend);
+        `uvm_info("Scoreboard", $sformatf("Remainder = %d", remainder),
+                  UVM_NONE)
     endfunction
 
     task run_phase(uvm_phase phase);
@@ -36,7 +46,6 @@ class scb extends uvm_scoreboard;
             wait (!(trs.size() == 0));
             tr = trs.pop_front();
             i++;
-            tr.print();
             compare(tr);
             `uvm_info("Tr count", $sformatf("Tr count = %d", i), UVM_NONE)
         end
