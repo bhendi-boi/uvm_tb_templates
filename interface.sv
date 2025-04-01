df0db1460e442a0d957300f80e90b68a7a390778
bhendi-boi <bhendiisgreen@gmail.com>
2025-04-01 09:12:59 +0530

    updated `interface.sv`

--- interface.sv
+++ interface.sv
@@ -1,3 +1,11 @@
-interface intf ();
-    // STEP 1: define your interface signals here
+interface intf (
+    input logic i_clk
+);
+    logic i_rst;
+    logic [31:0] i_dividend;
+    logic [31:0] i_divisor;
+    logic i_start;
+    logic o_ready;
+    logic o_quotient;
+    logic o_remainder;
 endinterface : intf
